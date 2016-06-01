b02c8d04be57c769b1aafb25c8fe23cf3f7e3852
nayeonkimmie <kimnaye@gmail.com>
2016-06-01 12:07:23 -0400

    update

--- unit2/js/app.js
+++ unit2/js/app.js
@@ -19,44 +19,33 @@ function displayFeed(myCurrentFeed) {
 				success: function(response) {
 					var myRedditObj = response.data.children;
 					myRedditObj.forEach(function(item) {
-						
+
 						//news feed
 						var eachArticle = '<article class="article">'; //each article
-							eachArticle += '<section class="featuredImage"><img src="images/article_placeholder_1.jpg" alt="" /></section>';//each objects have different struction for the images. How do I get images for its particular article?
+							eachArticle += '<section class="featuredImage"><img src="' + item.data.thumbnail + '" alt="" /></section>';
 							eachArticle += '<section class="articleContent"><a href="#"><h3>' + item.data.title + '</h3></a>';
 							eachArticle += '<h6>' + item.data.author + '</h6></section>'; //article content
 							eachArticle += '<section class="impressions">' + item.data.ups + '</section>'; //likes
-							eachArticle += '<div class="clearfix"></div></article>'; //clearfix 
+							eachArticle += '<div class="clearfix"></div></article>'; //clearfix
 						$('#main').append(eachArticle);
-						
+
 						//open popup
-						$('.article').click(function(item) {					  
-							var popupWindow = '<a href="#" class="closePopUp">x</a>';
-								popupWindow += '<div class="container">';
-								popupWindow += '<h1>' + /*item.data.title*/'Hi' +'</h1>';
-								popupWindow += '<p>' + /*item.data.secure_media.oembed.description*/'How are you?' + '</p>';
-								popupWindow += '<a href="#" class="popUpAction" target="_blank">Read more from source</a></div>';
+						var popupWindow = '<a href="#" class="closePopUp">x</a>';
+							popupWindow += '<div class="container">';
+							popupWindow += '<h1>' + item.data.title + '</h1>';
+							popupWindow += '<p>' + item.data + '</p>';
+							popupWindow += '<a href="' + item.data.url + '" class="popUpAction" target="_blank">Read more from source</a></div>';
+						$('.article').click(function() {
 							$('#popUp').append(popupWindow);
 							$('#popUp').show().removeClass('hidden loader');
 						});
-						
+
 						//close popup
 						$('div#popUp').on("click", "a.closePopUp", function() {
 							$('div#popUp').hide().addClass('hidden');
 						});
 					});
-										/*
-					$('#main').on("click", "article", function(item) {
-						$('div#popUp').show().removeClass('hidden');
-					
-						$('div.container').append($('<h1>'));
-						//$('h1').html(item.data.title);
-						
-						$('div.container').append($('<p>').html("Content of the article"));
-						
-						//?????? $('.loader').hide();
-					
-					
+					/*
 					<div id="popUp" class="loader hidden">
 						<a href="#" class="closePopUp">x</a>
 							<div class="container">
@@ -65,21 +54,21 @@ function displayFeed(myCurrentFeed) {
 								<a href="#" class="popUpAction" target="_blank">Read more from source</a>
 							</div>
 					</div>
-										
+
 					//search
 					$('#search').click(function() {
 						//upon clicking the search button, show the input field
 						$('#search').toggleClass('active');
 						//if ($('input').val() included keyword?) {
 							//show the keyword searched with articles that contain the keyword
-					});	
-				
+					});
+
 					*/
-					
-					
-					
-					
-				},			
+
+
+
+
+				},
 				error: function(response) {
 					console.log('error');
 				}
